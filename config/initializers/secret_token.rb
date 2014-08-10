02afd67e7b8122029526e1fc2f6438ab3555f031
Mark Lindamood <mark.lindamood@yahoo.com>
2014-08-09 18:07:29 -0700

    Adding Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AutoRating::Application.config.secret_key_base = 'f40b1a4c3881966e45ae23dcb04e2081f09aa4b20d5c3e9bdd85f91005df1f73ea4db8186b7c37fed949c5932e93f005dfbfc98fefd667104457156314ad753f'
