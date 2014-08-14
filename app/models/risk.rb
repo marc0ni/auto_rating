ce383140847964152cad89be2ecfe09b1fed8ab9
Mark Lindamood <mark.lindamood@yahoo.com>
2014-08-14 14:16:40 -0700

    Passing variables through partials

--- app/models/risk.rb
+++ app/models/risk.rb
@@ -1,6 +1,6 @@
 class Risk < ActiveRecord::Base
   belongs_to :submission
-  belongs_to :limits
-  belongs_to :deductibles
-  belongs_to :details
+  has_many :limits
+  has_many :deductibles
+  has_many :details
 end
